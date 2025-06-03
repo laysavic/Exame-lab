b78241b6609aeb112eab85420b3a4822c3d3507b
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-06-03 04:28:16 -0300

    conection

--- conexao.php
+++ conexao.php
@@ -11,23 +11,4 @@ function conectar(){
     }
 }
 
-// $localhost = "localhost";
-// $user = "root";
-// $password = "";
-// $banco = "exame-lab";
-
-// global $conexao;
-
-// try{
-//     $conexao = new PDO("mysql:dbname=".$banco."; host=" .$localhost, $user, $password);
-//     $conexao->setAttribute(
-//     PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//         echo "em breve algo aqui";
-
-// }catch(PDOException $e) {
-//     echo "ERRO na conexão ".$e->getMessage();
-//     exit;
-
-// }
-
 ?>
