bd39b20e8fe517861722a46c48913144a06b79fd
laysavic <laysa.alves8@aluno.ce.gov.br>
2025-06-12 20:16:39 -0300

    desenvolvimento do cadastro

---
+++ cadastro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro-Exame Lab</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="cadastro">
+    </div>
+    
+      <div class="register-box">
+        <img src="img/login.png" alt="" class="user-icon">
+        <h2>CADASTRO</h2>
+        <form class="form" action="logar.php" method="POST">
+        <div class="input-cad">
+          <label for="usuario">usuário:</label>
+          <input type="name" id="usuario" name="usuario"  placeholder="Ex: usuario"   required>
+          
+          <label for="email">e-mail:</label>
+          <input type="email" id="email" name="email"  placeholder="Ex: usuario@gmail.com"   required>
+          
+          <label for="senha">senha:</label>
+          <input type="password" id="senha" name="senha"  placeholder="Ex: 12345678" required>
+          
+        </div>
+
+
+        <div class=" btn" >
+            <button type="submit" >CADASTRAR</button>
+        </div>
+        </div>
+        
+    </div>
+</body>
+</html>
